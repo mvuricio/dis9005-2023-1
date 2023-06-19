d6077a4ebbb59e3b3d8e64329cb3a35c57391484
vicmunoz22 <128399607+vicmunoz22@users.noreply.github.com>
2023-06-19 12:15:16 -0400

    Update script.js

--- examen/vicmunoz22/script.js
+++ examen/vicmunoz22/script.js
@@ -1,5 +1,6 @@
 console.log("Bienvenidos a esta p&aacute;gina");
 console.log("ancho: " + window.innerWidth);
+// la fecha fue sacada de aqui https://www.w3schools.com/tags/tag_time.asp#:~:text=The%20tag%20defines%20a,can%20produce%20smarter%20search%20results.
 const d = new Date("2022-03-25");                             
 document.getElementById("fecha").innerHTML = d;
 function myFunction() {
@@ -7,6 +8,7 @@ function myFunction() {
     document.getElementById("fecha").style.color = "red";
     document.getElementById("fecha").style.backgroundColor = "yellow";        
   }
+  // lo siguiente fue sacado de clases por el profe
   console.log("probando");
 let martha = document.getElementById("inicio");
 console.log(martha);
@@ -21,6 +23,7 @@ function cambiarColor() {
 function cambiarTamano() {
   console.log("otro");
 }
+// lo siguiente viene de aca <!-- esto viene de aqui https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_onsubmit
 function myFunction() {
     var x = document.getElementById("myInput").value;
     document.getElementById("demo").innerHTML = "You wrote: " + x;
@@ -34,19 +37,19 @@ function myFunction() {
   alert("The form was submitted");
 }
 console.log("123");
+// esto fue el regalo del profe para la solemne 2 (los codigos)
 
 let formulario = document.getElementById("formulario");
 console.log(formulario);
 
-formulario.addEventListener("submit", Ingresar);
+formulario.addEventListener("submit", ingresar);
 
-function Ingresar(event) {
+function ingresar(event) {
   event.preventDefault();
   console.log("has ingresado");
-  document.getElementById("bienvenida").innerHTML = "hola tu " + Nombre.value;
+  document.getElementById("bienvenida").innerHTML = "hola tu " + nombre.value;
 }
-
-// inicio cita web https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow
+//lo siguiente viene de este link https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow_gallery
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -61,17 +64,18 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
+  slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
-
-// fin cita web https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow
+// lo del link termina aqui https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow_gallery
