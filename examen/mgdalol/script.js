c24334e3d38660ea94e394cde10b9c34b3b7ec1e
magdalena valdivia <128399618+mgdalol@users.noreply.github.com>
2023-06-19 12:59:51 -0400

    Update script.js

--- examen/mgdalol/script.js
+++ examen/mgdalol/script.js
@@ -1,20 +1,20 @@
-console.log("Hola solemne 2 tratame bien");
+console.log("Hola examen tratame bien");
 console.log("ancho: " + window.innerWidth);
 // como cambiar el tamaño y color de los textos sin cambiar el css a continuacion
 document.getElementById("personaje").style.color = "white";
-document.getElementById("personaje").style.fontSize = "20px";
-document.getElementById("personaje").style.fontFamily = "Georgia, 'Times New Roman', Times, serif";
+document.getElementById("personaje").style.fontSize = "25px";
+document.getElementById("personaje").style.fontFamily = "inherit";
 // importante agregar id dentro del texto que quiera editar para que sea encontrado dentro del js
 
 let formulario = document.getElementById("formulario");
 console.log(formulario);
 
-formulario.addEventListener("submit", registrarme);
-function registrarme(event) {
-    event.preventDefault();
-    console.log("haz añadido tu personaje");
-    document.getElementById("guardado").innerHTML = 
-    "hola" + escribeloaqui.value;
-    // intente hacer el form pero no funciona la respuesta despues de enviarlo
-    // el formulario lo obtuve por mi compañera sofia graciasuwu
- }
+ function validateForm() {
+    var x = document.forms["personajefav"]["fname"].value;
+    if (x == "" || x == null) {
+      alert("NO HAZ ESCRITO TU PERSONAJE FAVORTIO");
+      return false;
+    }
+  }
+//   intente linkear a algunas respuestas algunas paginas pero no pude:(
+    // y no pude profundizar tanto en el js porque profundice mas en el css
