80935318117e5a456963e9b1f62b85fb163f82bc
Camila-Parada <128399287+Camila-Parada@users.noreply.github.com>
2023-06-19 13:08:00 -0400

    Add files via upload

---
+++ examen/Camila-Parada/Examen final/script-main.js
@@ -0,0 +1,19 @@
+// Función de Google Analytics - Recuperado de: https://simonsepulveda.com/ (9-21)
+// Comentario: Buscaba inspirarme en la construcción de eta página y me pareció interesante aplicar este servicio de Google para obtener estadísticas.
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments)
+        }
+        ,
+        i[r].l = 1 * new Date();
+        a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    }
+    )(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-103151875-1', 'auto');
+// Fin de línea de Google Analytics
